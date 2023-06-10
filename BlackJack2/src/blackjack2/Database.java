67551da2a1758e430b1bd8954a1c4624eb07ddce
Yellowbunbunz <65571938+Yellowbunbunz@users.noreply.github.com>
2023-06-10 21:51:01 +1200

    Functioning Database

--- BlackJack2/src/blackjack2/Database.java
+++ BlackJack2/src/blackjack2/Database.java
@@ -7,7 +7,9 @@ package blackjack2;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database
 {
@@ -17,6 +19,7 @@ public class Database
     private static final String URL = "jdbc:derby://localhost:1527/BlackJackDB";  //url of the DB host
     
     Connection conn;
+    private Statement statement;
     
     public Database()
     {
@@ -46,25 +49,60 @@ public class Database
             System.out.println("Failed to connect to the database.");
             e.printStackTrace();
         }
-        
     }
     
-    public static void insertPlayerData(Connection connection, int playerID, String playerName, double playerCurrency) {
-        String sql = "INSERT INTO players (playerID, playerName, playerCurrency) VALUES (?, ?, ?)";
-        
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+    public void createCurrencyTable(Statement statement)
+    {
+        try {
+            // SQL statement to create the currency table
+            String sql = "CREATE TABLE Currency (" +
+                    "playerID INT PRIMARY KEY, " +
+                    "playerName VARCHAR(50), " +
+                    "playerCurrency DOUBLE" +
+                    ")";
             
+            statement.execute(sql);
+            
+            System.out.println("Currency table created.");
+        } catch (SQLException e) {
+            System.out.println("Failed to create the Currency table.");
+            e.printStackTrace();
+        }
+    }
+    
+    public void insertPlayerData(int playerID, String playerName, double playerCurrency)
+    {
+        String sql = "INSERT INTO Currency (playerID, playerName, playerCurrency) VALUES (?, ?, ?)";
+        
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
             statement.setInt(1, playerID);
             statement.setString(2, playerName);
             statement.setDouble(3, playerCurrency);
             
             int rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
+            if (rowsInserted > 0)
+            {
                 System.out.println("Player data inserted successfully!");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public int getLastAssignedPlayerID() {
+        try {
+            String sql = "SELECT MAX(playerID) FROM Currency";
+            Statement statement = conn.createStatement(); // Create a new statement object
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return 0;
     }
 }
 
--- BlackJack2/src/blackjack2/Game.java
+++ BlackJack2/src/blackjack2/Game.java
@@ -1,13 +1,14 @@
 /*
 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+*/
 package blackjack2;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 /**
@@ -15,7 +16,7 @@ import java.util.Scanner;
  * @author brook
  */
 public class Game {
-
+    
     //Instance Variables
     Player player;
     Dealer dealer;
@@ -23,7 +24,13 @@ public class Game {
     private FILEIO fileio;
     private double playerBalance;
     BlackJackGUI2 gui;
-
+    
+    private final Database database;
+    private final Connection conn;
+    private Statement statement;
+    
+    private int nextPlayerID = 1;
+    
     //Game Constructor Makes Player, Dealer and Deck.
     Game(String name, BlackJackGUI2 gui) throws IOException {
         fileio = new FILEIO("balances.txt");
@@ -32,33 +39,82 @@ public class Game {
         deck = new Deck();
         playerBalance = fileio.readBalance(name);
         this.gui = gui;
+        
+        database = new Database();
+        conn = database.getConnection();
+        
+        connectBlackJackDB(); // Establish the connection to the database
+        
+        this.nextPlayerID = database.getLastAssignedPlayerID() + 1;
     }
-
+    
+    public void connectBlackJackDB()
+    {
+        try {
+            // Establish the connection to the Blackjack database
+            conn.setAutoCommit(false); // Optional: Disable auto-commit for transaction control
+            
+            // Create a statement object to execute SQL queries
+            statement = conn.createStatement();
+            
+            // Perform any necessary database operations here
+            database.createCurrencyTable(statement);
+            
+            // If needed, you can commit the changes made to the database
+            // conn.commit();
+            
+            System.out.println("Connected to the Blackjack database.");
+            
+            this.nextPlayerID = database.getLastAssignedPlayerID() + 1;
+            
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the Blackjack database.");
+            e.printStackTrace();
+        }
+    }
+    
+    public void savePlayerData()
+    {
+        int playerID = generatePlayerID();
+        String playerName = player.getName();
+        double playerCurrency = player.getBalance();
+        
+        database.insertPlayerData(playerID, playerName, playerCurrency);
+    }
+    
+    public int generatePlayerID()
+    {
+        int newPlayerID = nextPlayerID;
+        nextPlayerID++;
+        return newPlayerID;
+    }
+    
     //Start method
-    public void Start() {
+    public void Start()
+    {
         deck.makeDeck();
         deck.shuffleCards();
         deal();
         play();
+        savePlayerData();
     }
-
+    
     //Play method.
-    public void play() {
-        
-        
+    public void play()
+    {
         boolean playersTurn = true;
-
+        
         /*gui.updatePlayerCards("\n" + player.getName() + "'s cards: \n" + player.getHand());
         gui.updateDealerHandValue("Hand Value: " + player.getHand().getValue());
         gui.updatePlayerBalance("Current Balance: " + playerBalance);
-
+        
         gui.updateDealerCards("\n" + dealer.getName() + "'s cards: \n" + dealer.getHand());
         gui.updateDealerHandValue("Hand Value: " + dealer.getHand().getValue());*/
         gui.showGamePanel();
         gui.updateUI();
-
+        
         player.hit = true;
-
+        
         while (playersTurn) {
             while (player.wantsToHit() == true && player.getHand().getValue() < 21) {
                 player.hit = false;
@@ -74,9 +130,9 @@ public class Game {
             }
             playersTurn = false;
         }
-
+        
         playersTurn = false;
-
+        
         System.out.println(dealer.wantsToHit());
         System.out.println(playersTurn);
         while (dealer.wantsToHit() == true && playersTurn == false) {
@@ -89,21 +145,21 @@ public class Game {
             }
         }
         gui.updateUI();
-
+        
         // Make sure balance doesn't go below zero
         if (player.getBalance() < 0) {
             player.setBalance(0);
         }
-
+        
         try {
             fileio.writeBalance(player.getName(), player.getBalance());
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
         }
-
+        
         System.out.println(player.getName() + " has a current balance of: " + player.getBalance());
     }
-
+    
     //Deals the cards
     public void deal() {
         player.getHand().addCard(deck.removeCard());
