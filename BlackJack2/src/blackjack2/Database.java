af0ae081434749edc929108a8c7aa1bcd0118528
Yellowbunbunz <65571938+Yellowbunbunz@users.noreply.github.com>
2023-06-10 20:18:45 +1200

    database connected

--- BlackJack2/src/blackjack2/Database.java
+++ BlackJack2/src/blackjack2/Database.java
@@ -5,21 +5,59 @@
 package blackjack2;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database
 {
-
-    public static void insertPlayerData(Connection connection, int playerID, String playerName, int playerCurrency) {
+    
+    private static final String userName = "BlackJackDB"; //your DB username
+    private static final String password = "BlackJackDB"; //your DB password
+    private static final String URL = "jdbc:derby://localhost:1527/BlackJackDB";  //url of the DB host
+    
+    Connection conn;
+    
+    public Database()
+    {
+        establishConnection();
+    }
+    
+    public static void main(String[] args)
+    {
+        Database database = new Database();
+        System.out.println(database.getConnection());
+    }
+    
+    public Connection getConnection()
+    {
+        return this.conn;
+    }
+    
+    //Establish connection
+    public void establishConnection()
+    {
+        try
+        {
+            conn = DriverManager.getConnection(URL, userName, password);
+            System.out.println("Connected to the database.");
+        } catch (SQLException e)
+        {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static void insertPlayerData(Connection connection, int playerID, String playerName, double playerCurrency) {
         String sql = "INSERT INTO players (playerID, playerName, playerCurrency) VALUES (?, ?, ?)";
-
+        
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
-
+            
             statement.setInt(1, playerID);
             statement.setString(2, playerName);
-            statement.setInt(3, playerCurrency);
-
+            statement.setDouble(3, playerCurrency);
+            
             int rowsInserted = statement.executeUpdate();
             if (rowsInserted > 0) {
                 System.out.println("Player data inserted successfully!");
